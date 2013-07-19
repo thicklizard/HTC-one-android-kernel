468d547d8d94617a0921754abe98f965a1594777
Thicklizard <thicklizard@gmail.com>
2013-07-19 17:39:53 -0500

    fix hsic wakelock

--- drivers/usb/core/driver.c
+++ drivers/usb/core/driver.c
@@ -1176,6 +1176,15 @@ int usb_suspend(struct device *dev, pm_message_t msg)
 {
 	struct usb_device	*udev = to_usb_device(dev);
 
+	if (udev->bus->skip_resume) {
+ 		if (udev->state == USB_STATE_SUSPENDED) {
+ 			return 0;
+ 		} else {
+ 			dev_err(dev, "abort suspend\n");
+ 			return -EBUSY;
+ 		}
+ 	}
+
 	unbind_no_pm_drivers_interfaces(udev);
 
 	choose_wakeup(udev, msg);
@@ -1195,6 +1204,9 @@ int usb_resume(struct device *dev, pm_message_t msg)
 {
 	struct usb_device	*udev = to_usb_device(dev);
 	int			status;
+
+	if (udev->bus->skip_resume)
+               return 0; 
 
 	pm_runtime_get_sync(dev->parent);
 	status = usb_resume_both(udev, msg);
